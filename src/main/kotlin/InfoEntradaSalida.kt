c6da0c2af36f0ce76318acb8b9a2766d99c6bf6c
Sara S Camilleri <ssancam391@g.educaand.es>
2024-04-13 15:06:00 +0200

    Creacion interfaces ejercicio 01

--- src/main/kotlin/InfoEntradaSalida.kt
+++ src/main/kotlin/InfoEntradaSalida.kt
@@ -1,25 +1,18 @@
 class InfoEntradaSalida : IinfoEntradaSalida {
-    override fun pedirOpcion(): Int? {
-        try{
-            do {
-                println("Introduce el número de tu opción:")
-                var opcion = readLine()
-                if (opcion?.toInt() in 1..<3){
-                    return opcion?.toInt()
-                }
-            }while (opcion == null || opcion.toInt() !in 1..<3)
-        }catch (e: IllegalArgumentException){
-            println("Opción inválida. Inténtalo de nuevo.")
-        }
-        return null
-    }
-
-    override fun pedirCadena() {
+    override fun pedirOpcion(): Int {
         TODO("Not yet implemented")
     }
 
-    override fun imprimirInfo(mensaje: String) {
-        return println(mensaje)
+    override fun pedirCadena(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun imprimirInfo(mensaje: Any): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun limpiar() {
+        TODO("Not yet implemented")
     }
 
 }
